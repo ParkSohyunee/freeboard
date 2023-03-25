b6023015d80bb7806bd11b9fc2eb910a6b1dced4
ParkSohyunee <sohyunee016@gmail.com>
2023-03-25 21:15:45 +0900

    feat: 댓글 삭제하기 기능 + 목록 refetch 기능 구현 완료

--- src/components/units/comment/list/CommentBoardList.container.js
+++ src/components/units/comment/list/CommentBoardList.container.js
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import BoardCommentListUI from "./CommentBoardList.presenter";
 
@@ -14,20 +14,51 @@ const FETCH_BOARD_COMMENTS = gql`
   }
 `;
 
+const DELETE_BOARD_COMMENT = gql`
+  mutation deleteBoardComment($password: String, $boardCommentId: ID!) {
+    deleteBoardComment(password: $password, boardCommentId: $boardCommentId)
+  }
+`;
+
 export default function BoardCommentList() {
   const router = useRouter();
-  // console.log(router.query.boardId);
+  // console.log(router);
 
   const { data } = useQuery(FETCH_BOARD_COMMENTS, {
     variables: { boardId: router.query.boardId },
   });
 
-  console.log(data); // {fetchBoardComments: Array(10)}
-  console.log(data?.fetchBoardComments); // [{…}, {…}, ..., {…}]
+  // console.log(data); // {fetchBoardComments: Array(10)}
+  // console.log(data?.fetchBoardComments); // [{…}, {…}, ..., {…}]
+
+  const [deleteBoardComment] = useMutation(DELETE_BOARD_COMMENT);
+
+  const onClickDeleteComment = async (event) => {
+    const password = prompt("비밀번호를 입력해 주세요.");
+    try {
+      await deleteBoardComment({
+        variables: {
+          password,
+          boardCommentId: event.target.id,
+        },
+        refetchQueries: [
+          {
+            query: FETCH_BOARD_COMMENTS,
+            variables: { boardId: router.query.boardId },
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
-      <BoardCommentListUI data={data} />
+      <BoardCommentListUI
+        data={data}
+        onClickDeleteComment={onClickDeleteComment}
+      />
     </>
   );
 }
--- src/components/units/comment/list/CommentBoardList.presenter.js
+++ src/components/units/comment/list/CommentBoardList.presenter.js
@@ -4,7 +4,7 @@ export default function BoardCommentListUI(props) {
   return (
     <>
       {props.data?.fetchBoardComments.map((el) => (
-        <S.Wrapper>
+        <S.Wrapper key={el._id}>
           <S.CommentBox>
             <S.Avatar src="/BoardComment/avatar.png/" />
             <S.MainWrapper>
@@ -15,7 +15,11 @@ export default function BoardCommentListUI(props) {
             </S.MainWrapper>
             <S.IconBox>
               <S.UpdateIcon src="/BoardComment/option_update_icon.png/" />
-              <S.DeleteIcon src="/BoardComment/option_delete_icon.png" />
+              <S.DeleteIcon
+                id={el._id}
+                onClick={props.onClickDeleteComment}
+                src="/BoardComment/option_delete_icon.png"
+              />
             </S.IconBox>
           </S.CommentBox>
           <S.DateBox>{el.createdAt}</S.DateBox>
