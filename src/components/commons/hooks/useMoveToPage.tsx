bc285de3e493daa3dfb3b7cea11c645a157b4510
ParkSohyunee <sohyunee016@gmail.com>
2023-04-10 17:32:21 +0900

    feat: 레이아웃 해더, 배너 스타일 수정 + 해더에 로그인, 회원가입 페이지 이동 기능 추가(custom-hooks 사용)

---
+++ src/components/commons/hooks/useMoveToPage.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export function useMoveToPage() {
+  const router = useRouter();
+
+  const onClickMoveToPage = (path: string) => () => {
+    void router.push(path);
+  };
+
+  return { onClickMoveToPage };
+}
--- src/components/commons/layout/header/index.tsx
+++ src/components/commons/layout/header/index.tsx
@@ -1,10 +1,52 @@
 import styled from "@emotion/styled";
+import { useMoveToPage } from "../../hooks/useMoveToPage";
+
+const Wrapper = styled.div`
+  height: 152px;
+`;
+export const InnerWrapper = styled.div`
+  padding: 1.8rem;
+  margin: auto;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const InnerLogo = styled.div`
+  font-size: 1.8rem;
+  font-weight: bold;
+  cursor: pointer;
+`;
+const RightNavbar = styled.div`
+  font-size: 1.25rem;
+  font-weight: 500;
+`;
+const InnerButton = styled.span`
+  padding: 0.8rem;
+  cursor: pointer;
+
+  :hover {
+    text-decoration: underline 0.3rem yellow;
+  }
+`;
 
 export default function LayoutHead() {
-  const Wrapper = styled.div`
-    height: 152px;
-    background-color: #f5f2fc;
-  `;
+  const { onClickMoveToPage } = useMoveToPage();
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <InnerLogo>메인아이콘</InnerLogo>
+        <RightNavbar>
+          <InnerButton onClick={onClickMoveToPage("/login")}>
+            로그인
+          </InnerButton>
+          <span>|</span>
+          <InnerButton onClick={onClickMoveToPage("/signup")}>
+            회원가입
+          </InnerButton>
+        </RightNavbar>
+      </InnerWrapper>
+    </Wrapper>
+  );
 }
