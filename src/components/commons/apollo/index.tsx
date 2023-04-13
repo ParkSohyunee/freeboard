37cfbd025cbf615d041a502a0e598b3c1566c688
ParkSohyunee <sohyunee016@gmail.com>
2023-04-13 17:39:27 +0900

    feat: 로그인과 Recoil 연결(2) - 로그인 토큰 apollo uploadLink headers에 추가 - 아폴로 캐시 글로벌 저장소에 저장

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -5,20 +5,28 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/store";
+
+const GLOBAL_STATE = new InMemoryCache();
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
 export default function ApolloSettingPage(props: IApolloSettingProps) {
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
   const uploadLink = createUploadLink({
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
   });
 
   const client = new ApolloClient({
     // uri: "http://backendonline.codebootcamp.co.kr/graphql",
     link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
-    cache: new InMemoryCache(),
+    cache: GLOBAL_STATE,
+    connectToDevTools: true,
   });
 
   // prettier-ignore
