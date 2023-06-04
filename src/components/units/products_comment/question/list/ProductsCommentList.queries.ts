6d6781e50deb07729c1e620c59d1964205e5ac8c
ParkSohyunee <sohyunee016@gmail.com>
2023-06-04 14:38:58 +0900

    feat: 중고마켓 댓글 [삭제] 기능 추가

--- src/components/units/products_comment/question/list/ProductsCommentList.queries.ts
+++ src/components/units/products_comment/question/list/ProductsCommentList.queries.ts
@@ -13,3 +13,9 @@ export const FETCH_USEDITEM_QUESTIONS = gql`
     }
   }
 `;
+
+export const DELETE_USEDITEM_QUESTION = gql`
+  mutation deleteUseditemQuestion($useditemQuestionId: ID!) {
+    deleteUseditemQuestion(useditemQuestionId: $useditemQuestionId)
+  }
+`;
--- src/components/units/products_comment/question/list/ProductsCommentListItems.tsx
+++ src/components/units/products_comment/question/list/ProductsCommentListItems.tsx
@@ -1,17 +1,52 @@
 import { useState } from "react";
-import { getDate } from "../../../../../commons/utils/date";
+import { useRouter } from "next/router";
+import { useMutation } from "@apollo/client";
+import { message } from "antd";
 
+import { getDate } from "../../../../../commons/utils/date";
+import {
+  DELETE_USEDITEM_QUESTION,
+  FETCH_USEDITEM_QUESTIONS,
+} from "./ProductsCommentList.queries";
 import * as S from "./ProductsCommentList.styles";
 import { ICommentListItemsProps } from "./ProductsCommentList.types";
+import {
+  IMutation,
+  IMutationDeleteUseditemQuestionArgs,
+} from "../../../../../commons/types/generated/types";
 import ProductsCommentRegister from "../register/ProductsCommentRegister.contatiner";
 
 export default function ProductsCommentListItems(
   props: ICommentListItemsProps
 ) {
+  const router = useRouter();
+
   const [isEdit, setIsEdit] = useState(false);
+
+  const [deleteUseditemQuestion] = useMutation<
+    Pick<IMutation, "deleteUseditemQuestion">,
+    IMutationDeleteUseditemQuestionArgs
+  >(DELETE_USEDITEM_QUESTION);
 
   const onClickUpdateComment = () => {
     setIsEdit(true);
+  };
+
+  const onClickDeleteComment = (useditemId: String) => () => {
+    try {
+      void deleteUseditemQuestion({
+        variables: { useditemQuestionId: String(useditemId) },
+        refetchQueries: [
+          {
+            query: FETCH_USEDITEM_QUESTIONS,
+            variables: { useditemId: router.query.productId },
+          },
+        ],
+      });
+      message.success({ content: "댓글이 삭제되었습니다." });
+    } catch (error) {
+      if (error instanceof Error) message.error(error.message);
+    }
   };
 
   return (
@@ -32,8 +67,8 @@ export default function ProductsCommentListItems(
                 src="/BoardComment/option_update_icon.png/"
               />
               <S.DeleteIcon
-                // id={props.el._id}
-                // onClick={onClickDeleteCommentModal}
+                id={props.el._id}
+                onClick={onClickDeleteComment(props.el._id)}
                 src="/BoardComment/option_delete_icon.png"
               />
             </S.IconBox>
